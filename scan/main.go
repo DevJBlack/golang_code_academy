2e4de7992d2f7ca407d4310162a5029a85a181fe
Devin Black <Devinjamesblack@gmail.com>
2020-06-28 19:09:04 -0600

    added extra fixtures to the mini conversation

--- scan/main.go
+++ scan/main.go
@@ -6,9 +6,9 @@ func main() {
 	fmt.Println("How are you doing?")
 	var emotion string
 	fmt.Scan(&emotion)
-	if emotion == "good" {
-		fmt.Printf("That is great to hear that you are %v!", emotion)
-	} else if emotion == "bad" {
+	if emotion == "good" || emotion == "Good" || emotion == "Good!" || emotion == "good!" {
+		fmt.Printf("That is great to hear that you are %v", emotion)
+	} else if emotion == "bad" || emotion == "Bad" || emotion == "bad!" || emotion == "Bad!" {
 		fmt.Printf("I am sorry to hear that you are doing %v", emotion)
 	} else {
 		fmt.Println("That is neither good nor bad")
